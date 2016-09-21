ad8bda7fc4fb647f8b37718a0a0f1b2b974a2aa6
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2016-09-21 14:12:29 -0600

    stub in for process and manager process

---
+++ src/ProcessSentinel.go
@@ -0,0 +1,11 @@
+/*
+ * @file 	: ProcessSentinel.go
+ * @brief 	: Entry point for the service.
+ * @author 	: Manuel A. Rodriguez (manuel.rdrs@gmail.com)
+ */
+package main
+
+// --------------------------------------------------------------
+func main() {
+
+}
