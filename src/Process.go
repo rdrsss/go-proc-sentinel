882c0e6858739fb5d268387ac758492ceb8783ae
Manuel A. Rodriguez <manuel@illfonic.com>
2016-09-21 14:12:29 -0600

    stub in for process and manager process

---
+++ src/Process.go
@@ -0,0 +1,20 @@
+/*
+ * @file 	: Process.go
+ * @brief 	: Describes a process, it's settings and metrics.
+ * @author 	: Manuel A. Rodriguez (manuel.rdrs@gmail.com)
+ */
+package main
+
+type Process struct {
+	PID string
+}
+
+func (p *Process) StartProcess() error {
+
+	return nil
+}
+
+func (p *Process) StopProcess() error {
+
+	return nil
+}
