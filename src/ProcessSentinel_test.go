fa296ae25a381220879a83a907ee49c42dead4d6
Manuel A. Rodriguez <manuel@illfonic.com>
2016-09-21 14:45:40 -0600

    stub in test file

---
+++ src/ProcessSentinel_test.go
@@ -0,0 +1,20 @@
+/*
+ * @file 	: ProcessSentinel_test.go
+ * @brief 	: Tests
+ * @author 	: Manuel A. Rodriguez (manuel.rdrs@gmail.com)
+ */
+package main
+
+import (
+	"testing"
+)
+
+// --------------------------------------------------------------
+func Test_SingleProgramStart(t *testing.T) {
+
+}
+
+// --------------------------------------------------------------
+func TestMain(m *testing.M) {
+	m.Run()
+}
